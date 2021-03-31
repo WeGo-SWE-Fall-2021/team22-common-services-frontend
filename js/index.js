4cb1c229fbd586e7ccfd186efc65bfa91bf05991
ErrorErrorError <erikbautista15@gmail.com>
2021-03-31 04:11:27 -0500

    Add support to check if a logged in session is already in place

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
-/* Adds shadow on header once it passes height */
+/* Global Variables */
+let cloud = window.location.hostname.split('.')[0]
+let cloudUrl = `https://${cloud}.team22.sweispring21.tk`
 
+
+/* Adds shadow on header once it passes height */
 $(window).scroll(() => {
     let header = $("header")
     if (window.scrollY > 0) {
@@ -40,6 +44,30 @@ $(() => {
             $(this).removeClass('text-white');
             $(this).addClass('text-secondary');
             $('#navbar').removeClass('navbar-mobile');
+        }
+    });
+})
+
+// This function will check to see if there is credentials saved. If so just direct them to the dashboard
+$(() => {
+    // This will validate the token if there is any to the server.
+    let headers = new Headers();
+    let cloud = window.location.hostname.split('.')[0]
+    fetch(cloudUrl + `/api/v1/common-services/user?cloud=${cloud}`, {
+        method:"GET",
+        headers: headers,
+
+    }).then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        return Promise.reject(response);
+    }).then(() => {
+        window.location.assign(cloudUrl + `/${cloud}-frontend/dashboard.html`)
+    }).catch(error => {
+        // If it fails to fetch user then redirect them to login
+        if (error.status === 401) {
+            window.location.assign("/login.html");
         }
     });
 })
